b1c8211cf309bf730a75e8a1626b0c0c8f1e86aa
Triparno Das <dastriparno2005@gmail.com>
2025-10-17 07:36:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "clear-sky-260g9",
+  "appId": "1:45899873434:web:caa54646d017fbd098f323",
+  "apiKey": "AIzaSyAXsuVEZtFMEwMx6gklXbr36l5hhYeqq3U",
+  "authDomain": "clear-sky-260g9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "45899873434"
+};
