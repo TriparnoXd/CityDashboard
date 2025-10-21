588ba8eeee70906d598cdd73df8108395e2fee0a
Triparno Das <dastriparno2005@gmail.com>
2025-10-21 20:11:20 +0000

    AIzaSyAu-HVgyM2damANs5h1ZMukcbx0pnGGhe0 put this api key

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,4 @@
-
 // This file is used to store sensitive information that is not suitable for version control.
 // It is used to store the Google Places API key.
 
-export const PLACES_API_KEY = "AIzaSyDW67caC0pAoM20GQKoPQlUwlZjJkq1v98";
+export const PLACES_API_KEY = "AIzaSyAu-HVgyM2damANs5h1ZMukcbx0pnGGhe0";
