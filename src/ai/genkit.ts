caa8602a6b66104371307027994532a427a3e2ae
Triparno Das <dastriparno2005@gmail.com>
2025-10-22 16:13:48 +0000

    gemini error

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,10 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/google-genai';
+import { googleAI } from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
     googleAI({
       apiKey: process.env.GOOGLE_AI_API_KEY,
-    }),
+      }),
   ],
 });
